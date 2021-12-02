0a430ad9de61b04fd00eb58e88f18c74300c628c
priyanshiisingh <priyanshianamika1815@gmail.com>
2021-12-02 19:22:48 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config("../.env");
+import connectdb from "./services/mongodb/models/connectDB";
 
 const app = express();
 
-app.listen(3000, (req, res) => {
-  console.log("Server listening to PORT 3000");
+const PORT = process.env.PORT || 3003;
+
+connectdb();
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server listening to PORT ${PORT}`);
 });
