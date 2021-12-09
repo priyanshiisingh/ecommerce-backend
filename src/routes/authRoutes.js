780eceb6f3ad97e4365815285931f3d81e21d005
priyanshiisingh <priyanshianamika1815@gmail.com>
2021-12-09 18:29:42 +0530

    changes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,9 +2,39 @@ import express from "express";
 import User from "../services/mongodb/models/User";
 const router = express.Router();
 
+/*
+type : GET
+path : /api/v1/auth/users 
+params: none
+isProtected : true (admin) 
+*/
+
 router.get("/users", async (req, res) => {
-  const users = await User.find({});
-  res.json({ users });
+  try {
+    const users = await User.find({});
+    res.json({ users });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ users: {} });
+  }
+});
+
+/*
+type : POST
+path : /api/v1/auth/signup
+params: none
+isProtected : false (admin) 
+*/
+
+router.post("/signup", async (req, res) => {
+  try {
+    const { firstName, lastName, email, password } = req.body;
+    const user = new User({ firstName, lastName, email, password });
+    res.json({ user });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ users: {} });
+  }
 });
 
 export default router;
--- src/services/mongodb/models/User.js
+++ src/services/mongodb/models/User.js
@@ -18,6 +18,10 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  role: {
+    type: Number,
+    default: 0,
+  },
 });
 
 const User = mongoose.model("User", UserSchema);
